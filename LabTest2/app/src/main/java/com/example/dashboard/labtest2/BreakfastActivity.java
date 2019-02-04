8e7703df504dbe0430fc856ce460c445d50a5243
piyushsingh27 <pyushsingh27@gmail.com>
2019-02-05 00:03:57 +0530

    lab test2

---
+++ LabTest2/app/src/main/java/com/example/dashboard/labtest2/BreakfastActivity.java
@@ -0,0 +1,15 @@
+package com.example.dashboard.labtest2;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BreakfastActivity extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.breakfast, container, false);
+    }
+}
--- NotificationLab/app/src/main/java/com/example/notificationlab/MainActivity.java
+++ NotificationLab/app/src/main/java/com/example/notificationlab/MainActivity.java
@@ -14,15 +14,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.app.NotificationChannel;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
     Button b1;
+
+    ImageView image;
+
     private final String CHANNEL_ID = "personal_notifications";
     private final int NOTIFICATION_ID = 001;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        image = findViewById(R.id.imageview);
 
         b1 = (Button)findViewById(R.id.button);
         b1.setOnClickListener(new View.OnClickListener() {
@@ -57,40 +63,43 @@ public class MainActivity extends Activity {
         manager.notify(0, builder.build());
     }
 
-    private void createNotificationChannel()
-    {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-        {
-            CharSequence name = "Personal Notifications";
-            String description = "Include all the personal notifications";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-
-            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, name, importance);
-
-            notificationChannel.setDescription(description);
-
-//            new AudioAttributes.Builder().setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-//                    .setLegacyStreamType(AudioManager.STREAM_NOTIFICATION)
-//                    .setUsage(AudioAttributes.USAGE_NOTIFICATION_EVENT)
-//                    .build();
-
-            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-            notificationManager.createNotificationChannel(notificationChannel);
-        }
-
+//    private void createNotificationChannel()
+//    {
 //        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
 //        {
-//            String channelId = "YOUR CHANNEL_ID";
-//            NotificationChannel channel = new NotificationChannel(channelId,
-//                    "Channel human readable title",
-//                    NotificationManager.IMPORTANCE_DEFAULT);
+//            CharSequence name = "Personal Notifications";
+//            String description = "Include all the personal notifications";
+//            int importance = NotificationManager.IMPORTANCE_DEFAULT;
 //
-//            new AudioAttributes.Builder().setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-//                    .setLegacyStreamType(AudioManager.STREAM_NOTIFICATION)
-//                    .setUsage(AudioAttributes.USAGE_NOTIFICATION_EVENT).build();
-//            mNotificationManager.createNotificationChannel(channel);
-//            mBuilder.setChannelId(channelId);
+//            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+//
+//            notificationChannel.setDescription(description);
+//
+////            new AudioAttributes.Builder().setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+////                    .setLegacyStreamType(AudioManager.STREAM_NOTIFICATION)
+////                    .setUsage(AudioAttributes.USAGE_NOTIFICATION_EVENT)
+////                    .build();
+//
+//            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+//            notificationManager.createNotificationChannel(notificationChannel);
 //        }
+
+//    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
 
